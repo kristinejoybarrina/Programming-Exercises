763f354c92c5471922b91aef91029b74501a4405
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-09-06 15:51:09 +0800

    Answered 5

--- ques5.py
+++ ques5.py
@@ -4,3 +4,20 @@
 # Question: Define a class which has at least two methods: getString: to get a string from console input printString: to print the string in upper case. Also please include simple test function to test the class methods.
 
 # Hints: Use init method to construct some parameters
+
+class String:
+    
+    def __init__(self):
+        self.__string = ""
+        
+    def getString(self):
+        self.__string = str(input("Enter a string: "))
+        return self.__string
+    
+    def printString(self):
+        return print (self.__string)
+    
+my_string = String()
+
+my_string.getString()
+my_string.printString()
